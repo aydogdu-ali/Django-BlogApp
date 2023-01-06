b42515e978676fc749df7299e6b2a99feefd0907
Ali <ali.ozgur@example.com>
2023-01-06 14:57:52 +0300

    apps added to settings

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my apps
+    'blog', #kendi applerimizi ekliyoruz.
+
+    #third party lib
+    'rest_framework', # 3. parti uygulamarı ekliyoruz.
 ]
 
 MIDDLEWARE = [
