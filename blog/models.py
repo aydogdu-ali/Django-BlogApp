a77346bd948779af7f2b5dda3c1b96bdd63b3bc1
Ali <ali.ozgur@example.com>
2023-01-06 16:01:41 +0300

    blogapp model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True) #Aynı kategoriden 1 den fazla olamaz.
+
+    def __str__(self): #tabloda ismi gözükmesi için yazdım.
+        return self.name
+
+
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete= models.PROTECT) #foreingkey onetomany ilişki demektir. Yani blogdaki yazılar aynı categoriye sahip olabilir demektir.
+    #BURDA CATEGORY SİLİNİRSE BLOG SİLİNMESİN KORU ANLAMINDADIR.
+    is_published = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
