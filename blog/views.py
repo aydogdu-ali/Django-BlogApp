46ba0d2657c138e7c906b99ddb4c126c92e35b19
Ali <ali.ozgur@example.com>
2023-01-06 17:55:10 +0300

    edit serializers.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from.models import Category, Blog
+from .serializers import CategorySerializers,BlogSerializers
+
+
+from rest_framework.viewsets import ModelViewSet
+
+#viewset modelini kullanıyorum
+class CategoryView(ModelViewSet):  #crud işlemleri
+    queryset = Category.objects.all() # kullanacağımız veri tablosu
+    serializer_class = CategorySerializers # kullanacağımız veri tablosuna ait serializer
+
+
+#viewset modelini kullanıyorum
+class BlogView(ModelViewSet): ##crud işlemleri
+    queryset = Blog.objects.all() # kullanacağımız veri tablosu
+    serializer_class = BlogSerializers # kullanacağımız veri tablosuna ait serializer
