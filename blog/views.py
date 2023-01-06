a95019337d853a50ba23c4f9d80255f558ac4ad3
Ali <ali.ozgur@example.com>
2023-01-06 20:24:48 +0300

    add user app

--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,18 @@ from rest_framework.viewsets import ModelViewSet
 class CategoryView(ModelViewSet):  #crud işlemleri
     queryset = Category.objects.all() # kullanacağımız veri tablosu
     serializer_class = CategorySerializers # kullanacağımız veri tablosuna ait serializer
+    
+    filterset_fields = ['name'] #modelde yazdığımız fields göre hangisi olacaksa onu yazıyoruz.
+
+    
 
 
 #viewset modelini kullanıyorum
 class BlogView(ModelViewSet): ##crud işlemleri
     queryset = Blog.objects.all() # kullanacağımız veri tablosu
     serializer_class = BlogSerializers # kullanacağımız veri tablosuna ait serializer
+    # filterset_fields = ['category__name'] # CATEGORY NAME İNE GÖRE FİLTRELEME YAPMAK İÇİN
+
+    filterset_fields = ['category'] #modelde yazdığımız fields göre hangisi olacaksa onu yazıyoruz.
+
+    search_fields= ['title', 'content'] #modelde yazdığımız fields göre hangisi olacaksa onu yazıyoruz.
--- main/settings.py
+++ main/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
 
     #my apps
     'blog', #kendi applerimizi ekliyoruz.
+    'user',
 
     #third party lib
     'rest_framework', # 3. parti uygulamarı ekliyoruz.
+    'django_filters', # 3. parti uygulamarı ekliyoruz.
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,35 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+        #pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    #filter
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    #seraching
+    'rest_framework.filters.SearchFilter'
+        
+    ]
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#     ],
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication',
+#     ]
